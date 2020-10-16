93ffa942ea5ecd4c3c8619091b25aba6df37e803
Raul Garcia <raul.gsouza@gmail.com>
2020-10-16 15:41:27 -0300

    Homepage sidebar and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
-import { Container } from '@/styles/pages/Home';
+import { Aside, Container, Content, Title } from '@/styles/pages/Home';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <h1>Home</h1>
+      <Aside>
+        <div>
+          <h4>Contato</h4>
+          <span>(22) 99888-6046</span>
+          <span>raul.gsouza@gmail.com</span>
+        </div>
+        <div>
+          <h4>Endereço</h4>
+          <span>R. Antonio Oliveira Gama, 202 </span>
+          <span>Cabo Frio/RJ</span>
+        </div>
+      </Aside>
+      <Content>
+        <Title>
+          <h1>Raul Pinto Coelho Garcia de Souza</h1>
+          <span>Desenvolvedor | Node.JS, ReactJS, React Native</span>
+        </Title>
+      </Content>
     </Container>
   );
 };
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,3 +1,59 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Aside = styled.aside`
+  height: calc(100% - 100px);
+  width: 320px;
+  padding: 36px;
+  background: #15b1fd;
+
+  border-radius: 15px 0 0 15px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+    h4 {
+      font-weight: 700;
+      margin-bottom: 16px;
+    }
+
+    span {
+      margin-bottom: 8px;
+    }
+  }
+
+  .adress {
+    strong {
+    }
+  }
+`;
+
+export const Content = styled.div`
+  height: calc(100% - 100px);
+  padding: 36px;
+  border-radius: 0 15px 15px 0;
+
+  background: #c3c3c3;
+`;
+
+export const Title = styled.header`
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    margin-bottom: 8px;
+  }
+`;
