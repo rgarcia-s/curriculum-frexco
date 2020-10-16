f8e943d50e8a579abb2bdd5a9a0c46722624c810
Raul Garcia <raul.gsouza@gmail.com>
2020-10-16 16:21:58 -0300

    Homepage content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
-import { Aside, Container, Content, Title } from '@/styles/pages/Home';
+import {
+  Aside,
+  Container,
+  Content,
+  Experiences,
+  Main,
+  Title,
+} from '@/styles/pages/Home';
 
 const Home: React.FC = () => {
   return (
@@ -20,6 +27,122 @@ const Home: React.FC = () => {
           <h1>Raul Pinto Coelho Garcia de Souza</h1>
           <span>Desenvolvedor | Node.JS, ReactJS, React Native</span>
         </Title>
+
+        <Main>
+          <div>
+            <h2>Sobre mim</h2>
+            <p>
+              Estudante de desenvolvimento focado na stack javascript Node.js,
+              ReactJS e React Native. Buscando primeiras experiências
+              profissionais em desenvolvimento e trabalho remoto.
+            </p>
+            <p>
+              Tive meu primeiro contato com a programação na faculdade de
+              Sistemas de Informação em 2013 estudando C++, Java e bancos de
+              dados SQL. Em 2019 voltei a focar meus estudos na programação
+              entrando no Bootcamp GoStack da Rocketseat. Nesse bootcamp aprendi
+              a criar uma aplicação do zero, desenvolvendo seu backend e
+              frontend web e mobile, conexão com bancos de dados relacionais e
+              não relacionais, testes e deploy.
+            </p>
+          </div>
+          <div>
+            <h2>Habilidades</h2>
+            <ul>
+              <li>
+                <img src="/berry.svg" alt="" />
+                Typescript
+              </li>
+              <li>
+                <img src="/berry.svg" alt="" />
+                Node.js
+              </li>
+              <li>
+                <img src="/berry.svg" alt="" />
+                ReactJS
+              </li>
+              <li>
+                <img src="/berry.svg" alt="" />
+                Next.js
+              </li>
+              <li>
+                <img src="/berry.svg" alt="" />
+                React Native
+              </li>
+              <li>
+                <img src="/berry.svg" alt="" />
+                Docker
+              </li>
+              <li>
+                <img src="/berry.svg" alt="" />
+                Git
+              </li>
+              <li>
+                <img src="/berry.svg" alt="" />
+                Inglês
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Formação</h2>
+
+            <ul className="formation">
+              <li>
+                <strong>Rocketseat</strong>
+                <span>Bootcamp Node.js, ReactJS, React Native</span>
+              </li>
+              <li>
+                <strong>Universidade estácio de sá</strong>
+                <span>Sistemas de informação - Previsão de conclusão 2021</span>
+              </li>
+            </ul>
+          </div>
+        </Main>
+        <Experiences>
+          <h2>Projetos</h2>
+          <ul>
+            <li>
+              <strong>
+                <a href="https://github.com/rgarcia-s/Rocketseat-GoBarber">
+                  GoBarber
+                </a>
+              </strong>
+
+              <p>
+                Sistema de agendamento de serviços para barbearias construído
+                com Node.js, ReactJS e React Native
+              </p>
+            </li>
+            <li>
+              <strong>
+                <a href="https://github.com/rgarcia-s/rocketseat-gympoint">
+                  Gympoint
+                </a>
+              </strong>
+
+              <p>
+                Sistema de agendamento de gerenciamento de alunos para academias
+                construído com Node.js, ReactJS e React Native
+              </p>
+            </li>
+            <li>
+              <strong>
+                <a href="https://github.com/rgarcia-s/Movie-Manager">
+                  Movie Manager
+                </a>
+
+                <span> - </span>
+
+                <a href="https://movie-manager.vercel.app/">Live Preview</a>
+              </strong>
+
+              <p>
+                Website para visualização de dados de filmes e séries construído
+                com Next.js e conexão com api externa. (Em construção)
+              </p>
+            </li>
+          </ul>
+        </Experiences>
       </Content>
     </Container>
   );
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100vw;
-  height: 100vh;
 
   display: flex;
   align-items: center;
@@ -10,8 +9,9 @@ export const Container = styled.div`
 `;
 
 export const Aside = styled.aside`
-  height: calc(100% - 100px);
+  height: 1400px;
   width: 320px;
+  margin: 40px 0;
   padding: 36px;
   background: #15b1fd;
 
@@ -42,7 +42,8 @@ export const Aside = styled.aside`
 `;
 
 export const Content = styled.div`
-  height: calc(100% - 100px);
+  height: 1400px;
+  max-width: 700px;
   padding: 36px;
   border-radius: 0 15px 15px 0;
 
@@ -55,5 +56,70 @@ export const Title = styled.header`
 
   h1 {
     margin-bottom: 8px;
+  }
+`;
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    margin: 40px 0 16px;
+    font-size: 24px;
+  }
+
+  p {
+    line-height: 24px;
+    margin-top: 16px;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    list-style: none;
+
+    img {
+      width: 40px;
+      margin-right: 8px;
+    }
+  }
+
+  .formation {
+    li {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+
+      strong {
+        margin-bottom: 4px;
+      }
+
+      span {
+        margin-bottom: 24px;
+      }
+    }
+  }
+`;
+
+export const Experiences = styled.div`
+  margin-top: 16px;
+
+  display: flex;
+  flex-direction: column;
+
+  li {
+    margin-top: 16px;
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: #7519c1;
+
+    cursor: pointer;
+  }
+
+  p {
+    margin-top: 8px;
   }
 `;
